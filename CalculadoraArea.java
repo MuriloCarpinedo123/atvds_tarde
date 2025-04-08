47410057ec922207ead6971c53934ad1312b9ab4
MuriloCarpinedo123 <carpinedo.muri@gmail.com>
2025-04-08 15:11:44 -0300

    Create CalculadoraArea.java

---
+++ CalculadoraArea.java
@@ -0,0 +1,49 @@
+package figura;
+
+public class CalculadoraArea {
+    public static void main(String[] args) {
+
+        public void setAlturaQ(int alturaQ) {
+            this.alturaQ = alturaQ;
+        }
+        public void setBaseQ(int baseQ) {
+            this.baseQ = baseQ;
+        }
+        
+        public void setAlturaT(int alturaT) {
+            this.alturaT = alturaT;
+        }
+        public void setBaseT(int baseT) {
+            this.baseT = baseT;
+        }
+        
+        
+        System.out.println("escreva a base do triangulo:");
+
+        System.out.println("escreva a altura do triangulo");
+
+        System.out.println("escreva a base do quadrado:");
+
+        System.out.println("escreva a altura do quadrado:");
+
+        
+        public int getBaseT(){
+            return(baseT);
+        }
+       
+        public int getaAturaT(){
+            return(alturaT);
+        }
+        public int getBaseQ(){
+            return(baseQ);
+        }
+        public int getAlturaQ(){
+            return(alturaQ);
+        }
+
+
+        calc.areaQuadrado(baseQ*alturaQ);
+        calc.areaTriangulo((baseT*alturaT)/2);
+    }
+    
+}
