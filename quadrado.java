a90a6f521722365f27eacca78e2d740cba874d8c
MuriloCarpinedo123 <carpinedo.muri@gmail.com>
2025-04-08 15:23:42 -0300

    Update quadrado.java

--- quadrado.java
+++ quadrado.java
@@ -1,9 +1,12 @@
 package figura;
 
+//cirando método construtor para usar na calculadora
 public class quadrado {
-    public int baseQ;
+   //settando as variáveis do método construtor
+    public int baseQ; 
     public int alturaQ;
 
+    //settando onde o resultado da calculadora printara
     public int areaQuadrado() {
         return (alturaQ * baseQ);
     }
