90f4cf4019061087d879d691019111eae4d24fab
MuriloCarpinedo123 <carpinedo.muri@gmail.com>
2025-04-08 15:12:19 -0300

    Add files via upload

---
+++ quadrado.java
@@ -0,0 +1,11 @@
+package figura;
+
+public class quadrado {
+    public int baseQ;
+    public int alturaQ;
+
+    public int areaQuadrado() {
+        return (alturaQ * baseQ);
+    }
+
+}
---
+++ triangulo.java
@@ -0,0 +1,12 @@
+package figura;
+
+public class triangulo {
+   public int AlturaT;
+   public int BaseT;
+
+    public int areaTriangulo() {
+        return (AlturaT * BaseT);
+    }
+
+
+}
