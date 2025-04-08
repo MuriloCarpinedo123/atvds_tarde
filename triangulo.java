54248122c83f5e995dfbfb8b834cb4af0e2a28b3
MuriloCarpinedo123 <carpinedo.muri@gmail.com>
2025-04-08 15:25:02 -0300

    Update triangulo.java

--- triangulo.java
+++ triangulo.java
@@ -1,9 +1,11 @@
 package figura;
 
+//settando o método construtor que será usado na calculadora
 public class triangulo {
+   //settando os atributos
    public int AlturaT;
    public int BaseT;
-
+//settando o resultado da calculadora
     public int areaTriangulo() {
         return (AlturaT * BaseT);
     }
