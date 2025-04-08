0d4bef2646e5cbe701e37ddd3bbf77f203a84af4
MuriloCarpinedo123 <carpinedo.muri@gmail.com>
2025-04-08 15:18:21 -0300

    Update CalculadoraArea.java

--- CalculadoraArea.java
+++ CalculadoraArea.java
@@ -1,8 +1,9 @@
 package figura;
 
+
 public class CalculadoraArea {
     public static void main(String[] args) {
-
+        //encapsulando os metodos construtores
         public void setAlturaQ(int alturaQ) {
             this.alturaQ = alturaQ;
         }
@@ -17,7 +18,7 @@ public class CalculadoraArea {
             this.baseT = baseT;
         }
         
-        
+        //pedindo os numeros que serao usados nas operações
         System.out.println("escreva a base do triangulo:");
 
         System.out.println("escreva a altura do triangulo");
@@ -26,22 +27,21 @@ public class CalculadoraArea {
 
         System.out.println("escreva a altura do quadrado:");
 
-        
+        //gettando os numeros que o usuario colocou
         public int getBaseT(){
-            return(baseT);
+            return(baseT); //retornando a base do triangulo
         }
-       
         public int getaAturaT(){
-            return(alturaT);
+            return(alturaT); //retornando a altura do triangulo
         }
         public int getBaseQ(){
-            return(baseQ);
+            return(baseQ); //retornando a base do quadrado
         }
         public int getAlturaQ(){
-            return(alturaQ);
+            return(alturaQ); //retornando a altura do quadrado
         }
 
-
+        //definindo a função que vai mostrar o resultado pro usuario
         calc.areaQuadrado(baseQ*alturaQ);
         calc.areaTriangulo((baseT*alturaT)/2);
     }
